b0ff25679b328f153cd716d9365755057024ab96
Olivier Tshidibi <doliveratshidibi@gmail.com>
2025-07-25 12:48:15 +0100

    correction animation

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import React, { useEffect, useState } from 'react';
 import logo from "../assets/logo-pwa.png";
 import "./SplashScreen.css";
 
 const SplashScreen = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // Déclenche l’animation quand la page est chargée
+    window.addEventListener('load', () => {
+      setLoaded(true);
+    });
+
+    // Sécurité si React monte après load
+    if (document.readyState === 'complete') {
+      setLoaded(true);
+    }
+  }, []);
   return (
     <div className="splash-container">
-      <img src={logo} alt="Logo SelfPay" className="splash-logo" />
+      <img src={logo} alt="Logo SelfPay" className={`logo-animated ${loaded ? 'loaded' : ''}`} />
       <div className="text-center mt-4">
   <p className="text-xs  tracking-widest" style={{ color: '#f5f5f5' }}>
     from
@@ -25,6 +39,7 @@ const SplashScreen = () => {
 
 
     </div>
+    
   );
 };
 
