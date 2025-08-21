c36c5e74bf60e8d4cfa978e52e0a40c30937728a
Olivier Tshidibi <doliveratshidibi@gmail.com>
2025-08-21 21:31:46 +0100

    css panier.jsx

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b702db8f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.epanv0aar38"
+    "revision": "0.m39amd8ivbg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
